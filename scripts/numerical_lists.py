e84394bdea8e690db746c93c019296b68dceb570
Jacob Melendrez <jacob.n.melendrezW@gmail.com>
2024-12-21 20:58:28 -0500

    Numerical list practice.

---
+++ scripts/numerical_lists.py
@@ -0,0 +1,21 @@
+nums = list(range(5))
+
+print()
+print(nums)
+
+high_nums = list(range(95, 100))
+
+print() 
+print(high_nums)
+
+odds = list(range(1, 10, 2))
+
+print() 
+print(odds) 
+print() 
+
+nums = []
+
+for exp in range(5): 
+    nums.append(10**exp)
+print(nums) 
